9bdb6e2e70ed5c4eee2dcd0eaedda99a65a70a7f
Quentin CLEMENT <quentinclement.pro@gmail.com>
2020-03-17 10:26:27 +0100

    Support tvOS 11.0

---
+++ SwiftWebVTT-tvOS/SwiftWebVTT_tvOS.h
@@ -0,0 +1,19 @@
+//
+//  SwiftWebVTT_tvOS.h
+//  SwiftWebVTT-tvOS
+//
+//  Created by Quentin CLEMENT on 17/03/2020.
+//  Copyright © 2020 Mikhail Apurin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SwiftWebVTT_tvOS.
+FOUNDATION_EXPORT double SwiftWebVTT_tvOSVersionNumber;
+
+//! Project version string for SwiftWebVTT_tvOS.
+FOUNDATION_EXPORT const unsigned char SwiftWebVTT_tvOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftWebVTT_tvOS/PublicHeader.h>
+
+
--- SwiftWebVTT.podspec
+++ SwiftWebVTT.podspec
@@ -8,6 +8,7 @@ Pod::Spec.new do |s|
   s.source = { :git => 'https://github.com/auramagi/SwiftWebVTT.git', :tag => s.version }
   
   s.ios.deployment_target = '11.0'
+  s.tvos.deployment_target = '11.0'
   
   s.swift_version = '5.0'
   
